c210a1d59153d4e833ac78b6914d4d615dd00ad2
Ayumu <ayumutsubosaka@Ayumu-no-MacBook-Pro.local>
2018-07-23 14:16:50 +0900

    fix: scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-docker run --rm dropest \
-dropest -V -f -g [refdata-cellranger-mm10-1.2.0/genes/genes.gtf] -c /home/user/dropEst/configs/10x.xml [neurons_900_possorted_genome_bam.bam]
-###### gtfはURLを添付
+docker run --rm -v $1:/home/user/gtf -v $2:/home/user/bam vpetukhov/dropest \
+dropest -V -f -g /home/user/gtf/genes.gtf -c /home/user/dropEst/configs/10x.xml /home/user/bam/$3
+###### gtfはURLを添付, bam fileは解析したいもの ########
 
 docker run -it --rm -p 7788:8888 --name velocyto_tsubosaka -v ./:/home/rstudio/data --user root velocyto_dk
